897f9e85ce300c930b5f89e9ecc1b8229b391a49
Pratham Rohra <rohrapratham03@gmail.com>
2023-12-02 02:41:14 +0530

    fix: countdown

--- index.js
+++ index.js
@@ -2,50 +2,47 @@ let timer;
 let minutesInput = document.getElementById('minutes');
 let timerDisplay = document.getElementById('timer');
 let startTime;
-let elapsedTime = 0;
 const timerClass = timerDisplay.classList;
 
 function startTimer() {
     let minutes = parseInt(minutesInput.value);
     
-    if (isNaN(minutes) || minutes < 0) {
-        alert('Please enter a valid positive number.');
+    if (isNaN(minutes) || minutes <= 0) {
+        alert('Please enter a valid positive number for minutes.');
         return;
     }
 
-    timerDisplay.classList.remove("timer-done");
-    timerDisplay.classList.remove("timer");
-    clearInterval(timer);
-    startTime = Date.now() - elapsedTime;
+    // timerDisplay.classList.remove("timer-done");
+    // timerDisplay.classList.remove("timer");
+
+    let seconds = minutes * 60;
+    startTime = ( Date.now() + seconds * 1000 ) + 1000; //Adding 1 extra second (delay)
+
     timer = setInterval(updateTimer, 1000);
     // updateTimer();
-    
 }
 
 function updateTimer() {
-    elapsedTime = Math.floor((Date.now() - startTime) / 1000);
+    let currentTime = Math.floor((startTime - Date.now()) / 1000);
 
-    if (elapsedTime >= minutesInput.value * 60) {
+    if (currentTime <= 0) {
         stopTimer();
         alert('Time\'s Up ⏰!');
+        return;
     }
-    
-    if (elapsedTime >= minutesInput.value * 60 - 10){
-        if (!timerClass.contains("timer")) {
-            timerClass.add("timer");
-        }
-    }
-    
 
-    let displayMinutes = Math.floor(elapsedTime / 60);
-    let displaySeconds = elapsedTime % 60;
-
-
+    let displayMinutes = Math.floor(currentTime / 60);
+    let displaySeconds = currentTime % 60;
 
     displayMinutes = displayMinutes < 10 ? '0' + displayMinutes : displayMinutes;
     displaySeconds = displaySeconds < 10 ? '0' + displaySeconds : displaySeconds;
 
     timerDisplay.innerText = `${displayMinutes}:${displaySeconds}`;
+    if (currentTime <= 10){
+        if (!timerClass.contains("timer")) {
+            timerClass.add("timer");
+        }
+    }
 }
 
 function stopTimer() {
